46d66793ab868bdc3f1cde20d8b22fba3788b5d9
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-26 11:07:23 -0400

    ✨ Add Channel model

---
+++ models/channel.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar
+
+from cumplo_common.models.channel import ChannelConfiguration, ChannelType
+from pydantic import BaseModel
+
+Model = TypeVar("Model", bound=BaseModel)
+
+
+class Channel(ABC):
+    configuration: ChannelConfiguration
+    type_: ChannelType
+
+    def __init__(self, configuration: ChannelConfiguration) -> None:
+        self.configuration = configuration
+
+    @abstractmethod
+    def send(self, message: Model) -> None:
+        """
+        Writes the message about the given topic to be sent through the given channel
+        """
