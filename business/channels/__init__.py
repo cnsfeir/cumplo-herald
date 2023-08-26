c1511517257c1e6bb03b6979abbd8ba28973846a
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-26 11:08:28 -0400

    ✨ Add first channel logic

---
+++ business/channels/__init__.py
@@ -0,0 +1,23 @@
+from collections.abc import Iterator
+from importlib import import_module
+from logging import getLogger
+
+from cumplo_common.models.user import User
+
+from models.channel import Channel
+
+logger = getLogger(__name__)
+
+
+def import_channels(user: User) -> Iterator[Channel]:
+    """
+    Imports the channels for the given user.
+    """
+    for channel_type, channel_configuration in user.channels.items():
+        try:
+            module = import_module(f"channels.{channel_type}")
+            channel = getattr(module, "channel")
+            yield channel(channel_configuration)
+
+        except (ImportError, AttributeError) as exception:
+            raise NotImplementedError(f"Channel {channel_type} is not implemented.") from exception
---
+++ business/channels/ifttt.py
@@ -0,0 +1,42 @@
+# mypy: disable-error-code="override"
+
+import json
+
+from cumplo_common.integrations.cloud_tasks import create_http_task
+from cumplo_common.models.channel import ChannelType, IFTTTConfiguration
+from pydantic import Field
+
+from models.channel import Channel
+from models.message import Message
+from utils.constants import PUSH_NOTIFICATIONS_QUEUE
+
+
+class IFTTTMessage(Message):
+    value1: str = Field(..., alias="title")
+    value2: str = Field(..., alias="message")
+    value3: str = Field(..., alias="url")
+
+
+class IFTTT(Channel):
+    configuration: IFTTTConfiguration
+    type_ = ChannelType.IFTTT
+
+    @property
+    def url(self) -> str:
+        """Builds the IFTTT trigger URL to send the message to"""
+        return f"https://maker.ifttt.com/trigger/test/with/key/{self.configuration.key}"
+
+    def send(self, message: IFTTTMessage) -> None:
+        """
+        Sends the message to the user.
+        """
+        create_http_task(
+            url=self.url,
+            queue=PUSH_NOTIFICATIONS_QUEUE,
+            payload=json.loads(message.model_dump_json()),
+            task_id="IFTTT-WEBHOOK",
+            is_internal=False,
+        )
+
+
+channel = IFTTT  # pylint: disable=invalid-name
