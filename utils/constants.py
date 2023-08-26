ac29f3ff778b68b39e757dd50c5de9a7edec6ba0
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-26 11:09:13 -0400

    🔥 Remove unused constants

--- utils/constants.py
+++ utils/constants.py
@@ -4,12 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONFIGURATIONS_COLLECTION = os.getenv("CONFIGURATIONS_COLLECTION", "configurations")
-IS_TESTING = bool(os.getenv("IS_TESTING"))
 
-LOCATION = os.getenv("LOCATION", "us-central1")
+IS_TESTING = bool(os.getenv("IS_TESTING"))
 LOG_FORMAT = "\n [%(levelname)s] %(message)s"
-MAX_CONFIGURATIONS = int(os.getenv("MAX_CONFIGURATIONS", "3"))
-NOTIFICATIONS_COLLECTION = os.getenv("NOTIFICATIONS_COLLECTION", "notifications")
-PROJECT_ID = os.getenv("PROJECT_ID")
-USERS_COLLECTION = os.getenv("USERS_COLLECTION", "users")
+PUSH_NOTIFICATIONS_QUEUE = os.getenv("PUSH_NOTIFICATIONS_QUEUE", "push-notifications")
