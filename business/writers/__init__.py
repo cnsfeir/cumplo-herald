d609ded1c613feff2bcc0a5e4781c3d0768a2de6
Cristobal Sfeir <cnsfeir@uc.cl>
2023-08-26 20:14:42 -0400

    🐛 Fix writer import

--- business/writers/__init__.py
+++ business/writers/__init__.py
@@ -1,7 +1,7 @@
 from importlib import import_module
 from logging import getLogger
 
-from caseconverter import pascalcase
+from caseconverter import pascalcase, snakecase
 from cumplo_common.models.template import Template
 from cumplo_common.models.topic import Topic
 
@@ -14,6 +14,6 @@ def import_writer(topic: Topic, template: Template) -> type[Writer]:
     """
     Imports the writer class for the given topic and template
     """
-    module = import_module(f"business.writers.{topic}")
+    module = import_module(f"business.writers.{snakecase(topic)}")
     writer = getattr(module, pascalcase(f"{template}-{topic}-writer"))
     return writer
