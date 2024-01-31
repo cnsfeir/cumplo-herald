77ad6083592576428d7bd5f037789a815aa8c319
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-30 21:22:21 -0300

    ⚰️ Remove CORS middleware

--- cumplo_herald/main.py
+++ cumplo_herald/main.py
@@ -3,7 +3,6 @@ from logging import DEBUG, basicConfig, getLogger
 import google.cloud.logging
 from cumplo_common.dependencies.authentication import authenticate
 from fastapi import Depends, FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from cumplo_herald.routers import common
 from cumplo_herald.utils.constants import IS_TESTING, LOG_FORMAT
@@ -17,15 +16,5 @@ if not IS_TESTING:
 
 
 app = FastAPI()
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 app.include_router(common.router, dependencies=[Depends(authenticate)])
