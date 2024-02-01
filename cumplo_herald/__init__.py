6b40d211c413d825faf3b5d377ccaa288cc3da5c
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-31 21:02:28 -0300

    📝 Reformat the project description

--- cumplo_herald/__init__.py
+++ cumplo_herald/__init__.py
@@ -1,3 +1,5 @@
-"""A multi-channel notification service designed to keep Cumplo investors ahead with real-time alerts"""
+"""
+A multi-channel notification service designed to keep Cumplo investors ahead with real-time alerts
+"""
 
 __version__ = "0.2.0"
