326676065b5a89874582a242327f6d1ffbd98478
Cristóbal Sfeir <cnsfeir@uc.cl>
2025-01-18 17:56:25 -0300

    refactor: rename cache router

--- cumplo_herald/routers/cache.py
+++ cumplo_herald/routers/cache.py
@@ -10,18 +10,18 @@ from fastapi.requests import Request
 logger = getLogger(__name__)
 
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/cache")
 
 
-@router.post("/channels", status_code=HTTPStatus.OK)
-def _clear_users_cache(request: Request) -> None:
+@router.post("/users/clear", status_code=HTTPStatus.OK)
+def _clear_user_cache(request: Request) -> None:
     """Clear a specific user from the Firestore cache so it can be updated."""
     user = cast(User, request.state.user)
     logger.debug(f"Clearing user {user.id} from the cache")
     firestore.users.cache.remove(id_user=user.id, api_key=user.api_key)
 
 
-@router.post("/notifications", status_code=HTTPStatus.OK)
+@router.post("/users/notifications/update", status_code=HTTPStatus.OK)
 def _update_notifications(request: Request, update: User) -> None:
     """Update the notifications of a specific user."""
     user = cast(User, request.state.user)
